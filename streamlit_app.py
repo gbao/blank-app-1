66db8db20273266b40463414bf2abf4b0f52817c
gbao <gbao411@gmail.com>
2024-12-22 13:51:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import plotly.graph_objects as go
+from plotly import graph_objects as go
 
 #Function to process the uploaded Excel file
 def load_excel(file):
